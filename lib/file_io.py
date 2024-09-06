329e9e4e1ffe740881d13b48015ee0bf618092a0
bmgwaro <brian.gwaro@student.moringaschool.com>
2024-09-06 15:59:30 +0300

    fileio

--- lib/file_io.py
+++ lib/file_io.py
@@ -1,8 +1,25 @@
 def write_file(file_name, file_content):
-    pass
+    
+    file_name += ".txt"
+    
+    
+    with open(file_name, "w") as file:
+        file.write(file_content)
 
-def append_file(file_name, append_content):
-    pass
+
+def append_file(file_name, file_content):
+    
+    file_name += ".txt"
+    
+    
+    with open(file_name, "a") as file:
+        file.write(file_content)
+
 
 def read_file(file_name):
-    pass
+    
+    file_name += ".txt"
+    
+    
+    with open(file_name, "r") as file:
+        return file.read()
